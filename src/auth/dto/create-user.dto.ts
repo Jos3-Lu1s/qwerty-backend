096cb61cf12ca4ee944944a5efe0313e08e91fd7
JoseLuis <5joselbautistac5@gmail.com>
2026-07-03 17:03:06 -0600

    [auth] qwerty: agregar dto para usuarios

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  @Matches(
+    /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'The password must have a Uppercase, lowercase letter and a number'
+})
+  password!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fullName!: string;
+}
