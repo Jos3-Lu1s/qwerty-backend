f707a001f362b25899239e53c3003bb0c3fabb28
JoseLuis <5joselbautistac5@gmail.com>
2026-07-03 17:53:18 -0600

    [auth] qwerty: agregar funcionalidad de login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, BadRequestException, InternalServerErrorException, Logger } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto, UpdateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -50,6 +49,37 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email: email.toLowerCase().trim() },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        fullName: true,
+        isActive: true,
+        roles: true,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid (email)');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is inactive, talk to an admin');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid (password)');
+    }
+
+    const { password: _, ...result } = user;
+    return result;
   }
 
   private handleDbErrors(error: any) {
